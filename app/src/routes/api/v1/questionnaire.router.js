d3552108714751c3d8d6551e0c58519eb0ece860
Tiago Garcia <tiagojsag@gmail.com>
2019-12-13 11:53:24 +0100

    Basic testing support; add k8s resource quota and checks

--- app/src/routes/api/v1/questionnaire.router.js
+++ app/src/routes/api/v1/questionnaire.router.js
@@ -1,6 +1,5 @@
 const Router = require('koa-router');
 const logger = require('logger');
-const ErrorSerializer = require('serializers/errorSerializer');
 const QuestionnaireSerializer = require('serializers/questionnaireSerializer');
 const QuestionnaireModel = require('models/questionnaireModel');
 const QuestionnaireValidator = require('validators/questionnaireValidator');
@@ -14,19 +13,19 @@ const router = new Router({
 
 class QuestionnaireRouter {
 
-    static * getAll(){
+    static* getAll() {
         logger.info('Obtaining all questionnaires');
         const questionnaireModels = yield QuestionnaireModel.find();
         this.body = QuestionnaireSerializer.serialize(questionnaireModels);
     }
 
-    static * get(){
+    static* get() {
         logger.info(`Obtaining questionnaire with id ${this.params.id}`);
         const questionnaire = yield QuestionnaireModel.find({ _id: this.params.id });
         this.body = QuestionnaireSerializer.serialize(questionnaire);
     }
 
-    static * save(){
+    static* save() {
         logger.info('Saving questionnaire');
         const questionnaire = yield new QuestionnaireModel({
             name: this.request.body.name,
@@ -35,12 +34,12 @@ class QuestionnaireRouter {
         this.body = QuestionnaireSerializer.serialize(questionnaire);
     }
 
-    static * update(){
+    static update() {
         this.throw(500, 'Not implemented');
-        return;
+
     }
 
-    static * delete(){
+    static* delete() {
         logger.info(`Deleting questionnaire with id ${this.params.id}`);
         const result = yield QuestionnaireModel.remove({ _id: this.params.id });
         if (!result || !result.result || result.result.ok === 0) {
@@ -51,7 +50,7 @@ class QuestionnaireRouter {
         this.statusCode = 204;
     }
 
-    static * downloadAnswers() {
+    static* downloadAnswers() {
         logger.info(`Download answers of questionnaire ${this.params.id}`);
         this.set('Content-disposition', `attachment; filename=${this.params.id}.csv`);
         this.set('Content-type', 'text/csv');
@@ -63,11 +62,11 @@ class QuestionnaireRouter {
             this.throw(404, 'Not found');
             return;
         }
-        for (let i = 0, length = questionnaire.questions.length; i < length; i++) {
+        for (let i = 0, { length } = questionnaire.questions; i < length; i++) {
             const question = questionnaire.questions[i];
             questions[question.name] = null;
-            if (question.childQuestions){
-                for (let j = 0, lengthChild =question.childQuestions.length; j < lengthChild; j++ ){
+            if (question.childQuestions) {
+                for (let j = 0, lengthChild = question.childQuestions.length; j < lengthChild; j++) {
                     questions[question.childQuestions[j].name] = null;
                 }
             }
@@ -81,28 +80,29 @@ class QuestionnaireRouter {
             logger.debug('Data found!');
             let data = null;
 
-            for (let i = 0, length = answers.length; i < length; i++) {
+            for (let i = 0, { length } = answers; i < length; i++) {
                 const answer = answers[i];
-                const responses = Object.assign({}, questions);
-                for(let j = 0, lengthResponses = answer.responses.length; j < lengthResponses; j++){
+                const responses = { ...questions };
+                for (let j = 0, lengthResponses = answer.responses.length; j < lengthResponses; j++) {
                     const res = answer.responses[j];
                     responses[res.question] = res.value;
                 }
                 logger.debug('Writting...');
-                data = json2csv({
+                data = `${json2csv({
                     data: responses,
                     hasCSVColumnTitle: i === 0
-                }) + '\n';
+                })}\n`;
                 this.body.write(data);
             }
         }
         this.body.end();
     }
+
 }
 
 
-function * loggedUserToState(next) {
-    if (this.query && this.query.loggedUser){
+function* loggedUserToState(next) {
+    if (this.query && this.query.loggedUser) {
         this.state.loggedUser = JSON.parse(this.query.loggedUser);
         delete this.query.loggedUser;
     } else if (this.request.body && this.request.body.loggedUser) {
@@ -115,11 +115,23 @@ function * loggedUserToState(next) {
     yield next;
 }
 
-function * checkPermission(next) {
-    if (this.state.loggedUser.role === 'USER' || (this.state.loggedUser.role==='MANAGER' && (!this.state.loggedUser.extraUserData || this.state.loggedUser.extraUserData.apps || this.state.loggedUser.extraUserData.apps.indexOf('gfw') === -1))) {
+function* checkPermission(next) {
+    if (this.state.loggedUser.role === 'USER') {
         this.throw(403, 'Not authorized');
         return;
     }
+
+    if (
+        this.state.loggedUser.role === 'MANAGER' && (
+            !this.state.loggedUser.extraUserData
+            || this.state.loggedUser.extraUserData.apps
+            || this.state.loggedUser.extraUserData.apps.indexOf('gfw') === -1
+        )
+    ) {
+        this.throw(403, 'Not authorized');
+        return;
+    }
+
     yield next;
 }
 
