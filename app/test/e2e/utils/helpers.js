087e7b3159891c5f1122bee6a7516e115ddfa6fd
Tiago Garcia <tiagojsag@gmail.com>
2019-12-13 15:32:20 +0100

    Add tests for get reports and get report by id

---
+++ app/test/e2e/utils/helpers.js
@@ -0,0 +1,23 @@
+const Report = require('models/reportsModel');
+const { ROLES } = require('./test.constants');
+
+const getUUID = () => Math.random().toString(36).substring(7);
+
+
+const createReport = (additionalData = {}) => {
+    const uuid = getUUID();
+
+    return new Report({
+        name: `Report ${uuid}`,
+        user: ROLES.USER.id,
+        languages: ['en'],
+        defaultLanguage: 'en',
+        ...additionalData
+    }).save();
+};
+
+
+module.exports = {
+    createReport,
+    getUUID
+};
