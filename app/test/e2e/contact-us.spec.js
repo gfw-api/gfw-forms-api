70275b13033f7c76989fecde27a3e12c39da447c
Tiago Garcia <tiagojsag@gmail.com>
2020-04-09 17:37:05 +0200

    WIP

--- app/test/e2e/contact-us.spec.js
+++ app/test/e2e/contact-us.spec.js
@@ -78,8 +78,16 @@ describe('Contact us tests', () => {
             should.fail(error.message);
         });
 
-        nock('https://accounts.google.com')
-            .post('/o/oauth2/token',
+        // FetchError: request to https://www.googleapis.com/oauth2/v4/token failed, reason: Nock: Disallowed net connect for "www.googleapis.com:443/oauth2/v4/token"
+        //     at ErroringClientRequest.<anonymous> (/home/tiagojsag/web/gfw-forms-api/node_modules/node-fetch/lib/index.js:1455:11)
+        // at ErroringClientRequest.emit (events.js:311:20)
+        // at ErroringClientRequest.EventEmitter.emit (domain.js:482:12)
+        // at ErroringClientRequest.<anonymous> (/home/tiagojsag/web/gfw-forms-api/node_modules/nock/lib/intercept.js:235:12)
+        // at processTicksAndRejections (internal/process/task_queues.js:79:11)
+
+
+        nock('https://www.googleapis.com')
+            .post('/oauth2/v4/token',
                 (body) => {
                     if (body.grant_type !== 'urn:ietf:params:oauth:grant-type:jwt-bearer') {
                         return false;
@@ -94,6 +102,39 @@ describe('Contact us tests', () => {
                 expires_in: 3599,
                 token_type: 'Bearer'
             });
+
+        nock('https://www.googleapis.com:443', { encodedQueryParams: true })
+            .post('/oauth2/v4/token', { grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer', assertion: 'eyJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJnZnctdGVzdC11c2Vyc0BnZnctdGVzdGVycy5pYW0uZ3NlcnZpY2VhY2NvdW50LmNvbSIsInNjb3BlIjoiaHR0cHM6Ly93d3cuZ29vZ2xlYXBpcy5jb20vYXV0aC9zcHJlYWRzaGVldHMiLCJhdWQiOiJodHRwczovL3d3dy5nb29nbGVhcGlzLmNvbS9vYXV0aDIvdjQvdG9rZW4iLCJleHAiOjE1ODUzMTgxMDgsImlhdCI6MTU4NTMxNDUwOH0.W_nUMbl6cZn1NOyQvfsae2JvVoiO_6aB6Pw8MQG2hpds3xSoBZ_QKGzNBQpSHOy9AWvgRGzBXA4N_g_-ySc-5yCmNfAodL9suZOyObgJo7xyDQhlXiXZ_zV_sU6pG7Upl3mSqjw7X2aDcHEXPNmp_TqiQjcMJ7RIWMN11elREJuR9f48PKJnHp2vS_socWV0tBv_ZEVDP3e7u8jc0C2lyChlkg5-2xSWzpgrMLnUHKrKwf6XEer1v6LTH11j35nxwEwncVY_gWa-AXGWYWt0Kh8mF4AoLxJe4NhUL1YArqhjELEa8tVi_KKlQ7hleCUOWgwPFliH8_4Nr6XGjsPM5w' })
+            .reply(200, ['1f', '8b', '08', '00', '00', '00', '00', '00', '02', 'ff1dcf517244300000d0bbe47bbb534545ff2445b55b7487503f061bc576080942a777ef4edf0dde0f28aa8a729e8be14a7bf004b6e2c13c56c7b701226955a7357cddabe80b13558946c86d7f91d970b215c323a8c5d7d233f597c1b7d816c247273b1b96c163fcdee34ba82d1e4c88ecf1ba11d7fa5e345d7b8eb063acfb7c71f16c8c3677c74846fe596871906542373bd4ed3b7215a10e014f1a774126ac99722f611956194e538f12bcdd35e91027cd67ad7765bb361f910c7ab2d6652c47a7144c8d9b592f3c668103a092b513e5797b9ba9ba691ec07f33171ba3b72ba2c54427f0fb073100cafb05010000'], [
+                'Content-Type',
+                'application/json; charset=UTF-8',
+                'Vary',
+                'Origin',
+                'Vary',
+                'X-Origin',
+                'Vary',
+                'Referer',
+                'Content-Encoding',
+                'gzip',
+                'Date',
+                'Fri, 27 Mar 2020 13:08:29 GMT',
+                'Server',
+                'scaffolding on HTTPServer2',
+                'Cache-Control',
+                'private',
+                'X-XSS-Protection',
+                '0',
+                'X-Frame-Options',
+                'SAMEORIGIN',
+                'X-Content-Type-Options',
+                'nosniff',
+                'Alt-Svc',
+                'quic=":443"; ma=2592000; v="46,43",h3-Q050=":443"; ma=2592000,h3-Q049=":443"; ma=2592000,h3-Q048=":443"; ma=2592000,h3-Q046=":443"; ma=2592000,h3-Q043=":443"; ma=2592000,h3-T050=":443"; ma=2592000',
+                'Connection',
+                'close',
+                'Transfer-Encoding',
+                'chunked'
+            ]);
 
         nock('https://spreadsheets.google.com')
             .get(`/feeds/cells/${config.get('googleSheets.target_sheet_id')}/${config.get('googleSheets.target_sheet_index')}/private/full`)
@@ -137,7 +178,7 @@ describe('Contact us tests', () => {
         });
 
         nock('https://accounts.google.com')
-            .post('/o/oauth2/token',
+            .post('/oauth2/v4/token',
                 (body) => {
                     if (body.grant_type !== 'urn:ietf:params:oauth:grant-type:jwt-bearer') {
                         return false;
@@ -196,7 +237,7 @@ describe('Contact us tests', () => {
         });
 
         nock('https://accounts.google.com')
-            .post('/o/oauth2/token',
+            .post('/oauth2/v4/token',
                 (body) => {
                     if (body.grant_type !== 'urn:ietf:params:oauth:grant-type:jwt-bearer') {
                         return false;
@@ -265,7 +306,7 @@ describe('Contact us tests', () => {
         });
 
         nock('https://accounts.google.com')
-            .post('/o/oauth2/token',
+            .post('/oauth2/v4/token',
                 (body) => {
                     if (body.grant_type !== 'urn:ietf:params:oauth:grant-type:jwt-bearer') {
                         return false;
@@ -337,7 +378,7 @@ describe('Contact us tests', () => {
         });
 
         nock('https://accounts.google.com')
-            .post('/o/oauth2/token',
+            .post('/oauth2/v4/token',
                 (body) => {
                     if (body.grant_type !== 'urn:ietf:params:oauth:grant-type:jwt-bearer') {
                         return false;
