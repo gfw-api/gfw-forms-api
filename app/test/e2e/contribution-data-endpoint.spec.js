2145798fcbd67a75fc2f731ee4a59bb88fed5618
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2020-08-19 12:00:35 +0100

    Tests for contribution data endpoint

---
+++ app/test/e2e/contribution-data-endpoint.spec.js
@@ -0,0 +1,39 @@
+const nock = require('nock');
+const chai = require('chai');
+const sinon = require('sinon');
+
+const { getTestServer } = require('./utils/test-server');
+
+nock.disableNetConnect();
+nock.enableNetConnect(process.env.HOST_IP);
+
+chai.should();
+
+let requester;
+let sinonSandbox;
+
+describe('Contribution data endpoint tests', () => {
+    before(async () => {
+        if (process.env.NODE_ENV !== 'test') {
+            throw Error(`Running the test suite with NODE_ENV ${process.env.NODE_ENV} may result in permanent data loss. Please use NODE_ENV=test.`);
+        }
+
+        requester = await getTestServer();
+    });
+
+    beforeEach(() => { sinonSandbox = sinon.createSandbox(); });
+
+    it('Calling the contribution data endpoint returns 200 OK (happy case)', async () => {
+        const response = await requester.post(`/api/v1/form/contribution-data`).send();
+        response.status.should.equal(200);
+        response.body.should.equal('');
+    });
+
+    afterEach(async () => {
+        if (!nock.isDone()) {
+            throw new Error(`Not all nock interceptors were used: ${nock.pendingMocks()}`);
+        }
+
+        sinonSandbox.restore();
+    });
+});
