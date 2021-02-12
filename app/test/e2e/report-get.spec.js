8438571d34a26f167f2719f0fe33142263c90494
Tiago Garcia <tiagojsag@gmail.com>
2021-02-12 11:07:16 +0100

    Update tests

--- app/test/e2e/report-get.spec.js
+++ app/test/e2e/report-get.spec.js
@@ -31,7 +31,7 @@ describe('Get reports tests', () => {
         response.status.should.equal(401);
         response.body.should.have.property('errors').and.be.an('array').and.length(1);
         response.body.errors[0].should.have.property('status').and.equal(401);
-        response.body.errors[0].should.have.property('detail').and.equal('Not logged');
+        response.body.errors[0].should.have.property('detail').and.equal('Unauthorized');
     });
 
     it('Get all reports as an authenticated user should return an empty list', async () => {
